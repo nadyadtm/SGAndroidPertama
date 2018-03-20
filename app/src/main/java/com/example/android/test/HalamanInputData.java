0ff7bfcd83ccd32d73bdf0252aca735cc4b9fd12
nadyadtm <nadyadtm@github.com>
2018-03-20 21:34:56 +0700

    Ini Commit

---
+++ app/src/main/java/com/example/android/test/HalamanInputData.java
@@ -0,0 +1,13 @@
+package com.example.android.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HalamanInputData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_input_data);
+    }
+}
