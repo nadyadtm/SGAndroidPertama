71e9ba12b103002033f1e7c1ac451e9862068e0e
nadyadtm <nadyadtm@github.com>
2018-03-20 21:07:16 +0700

    Ini Commit

--- app/src/main/java/com/example/android/test/MainActivity.java
+++ app/src/main/java/com/example/android/test/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.test;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import static android.R.attr.delay;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent pindah = new Intent(MainActivity.this,Home.class);
+                startActivity(pindah);
+                finish();
+            }
+        }, 3000);
     }
 }
